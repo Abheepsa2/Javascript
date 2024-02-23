66017f5a4f1cf3e6f4865ff8bbe08b2086b06a04
Abheepsa Pradhan <abheepsapradhan123@gmail.com>
2024-02-23 18:30:01 +0545

    first commit

---
+++ day1.js
@@ -0,0 +1,22 @@
+// console.log("hellow world");
+// console.log("hi world");
+//  var cup="coffee"
+//  let cup1="coffee"
+//  const cup2="coffee"
+//  if (true){
+//     var name = "john doe"
+// console.log(name,"this is forst one")
+//  }
+//  console.log(name,"this is second one")
+//  const age=21
+//  const isloving =true
+// let name="Abheepsa "
+// console.log(name+ "Pradhan")
+let friends=['sofia','ayush','lucky',12,true]
+console.log(friends[1])
+
+let khayo={
+    name :"crafty",
+price : 999
+}
+console.log(khayo)
